61030b802478ee31bd951351a0eb82173a76e623
Dennny <keplerde@gmail.com>
2022-03-13 15:10:43 +0100

    Basket + Summury

--- core/basket/views.py
+++ core/basket/views.py
@@ -5,8 +5,11 @@ from store.models import Product
 
 from .basket import Basket
 
+
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
 
 def basket_add(request):
     basket = Basket(request)
@@ -18,4 +21,16 @@ def basket_add(request):
 
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
+        return response
+
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
