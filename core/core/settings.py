d6f961bc53fed650e567883141c932d822a17e28
Dennny <keplerde@gmail.com>
2022-03-15 18:06:33 +0100

    Finishing registration Form

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'basket',
+    'account',
 
 ]
 
@@ -139,3 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Basket session ID
 BASKET_SESSION_ID = 'basket'
+
+# Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
