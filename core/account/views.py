257801ae2b21c247c18b5f62af8cfe7bffcfe315
Dennny <keplerde@gmail.com>
2022-04-10 09:00:38 +0200

    Implement Paypal

--- core/account/views.py
+++ core/account/views.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from orders.views import Order
 from orders.views import user_orders
 from store.models import Product
 
@@ -162,3 +163,11 @@ def set_default(request, id):
         return redirect("checkout:delivery_address")
 
     return redirect("account:addresses")
+
+
+@login_required
+def user_orders(request):
+    user_id = request.user.id
+    orders = Order.objects.filter(user_id=user_id).filter(billing_status=True)
+    return render(request, "account/dashboard/user_orders.html", {"orders": orders})
+
--- core/checkout/views.py
+++ core/checkout/views.py
@@ -16,6 +16,7 @@ def deliverychoices(request):
     deliveryoptions = DeliveryOptions.objects.filter(is_active=True)
     return render(request, "checkout/delivery_choices.html", {"deliveryoptions": deliveryoptions})
 
+
 @login_required
 def basket_update_delivery(request):
     basket = Basket(request)
@@ -35,6 +36,7 @@ def basket_update_delivery(request):
 
         response = JsonResponse({"total": updated_total_price, "delivery_price": delivery_type.delivery_price})
         return response
+
 
 @login_required
 def delivery_address(request):
@@ -65,3 +67,52 @@ def payment_selection(request):
 
     return render(request, "checkout/payment_selection.html", {})
 
+
+####
+# PayPay
+####
+from paypalcheckoutsdk.orders import OrdersGetRequest
+
+from .paypal import PayPalClient
+
+
+@login_required
+def payment_complete(request):
+    PPClient = PayPalClient()
+
+    body = json.loads(request.body)
+    data = body["orderID"]
+    user_id = request.user.id
+
+    requestorder = OrdersGetRequest(data)
+    response = PPClient.client.execute(requestorder)
+
+    total_paid = response.result.purchase_units[0].amount.value
+
+    basket = Basket(request)
+    order = Order.objects.create(
+        user_id=user_id,
+        full_name=response.result.purchase_units[0].shipping.name.full_name,
+        email=response.result.payer.email_address,
+        address1=response.result.purchase_units[0].shipping.address.address_line_1,
+        address2=response.result.purchase_units[0].shipping.address.admin_area_2,
+        postal_code=response.result.purchase_units[0].shipping.address.postal_code,
+        country_code=response.result.purchase_units[0].shipping.address.country_code,
+        total_paid=response.result.purchase_units[0].amount.value,
+        order_key=response.result.id,
+        payment_option="paypal",
+        billing_status=True,
+    )
+    order_id = order.pk
+
+    for item in basket:
+        OrderItem.objects.create(order_id=order_id, product=item["product"], price=item["price"], quantity=item["qty"])
+
+    return JsonResponse("Payment completed!", safe=False)
+
+
+@login_required
+def payment_successful(request):
+    basket = Basket(request)
+    basket.clear()
+    return render(request, "checkout/payment_successful.html", {})
